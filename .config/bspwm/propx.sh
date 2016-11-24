ea9e89e189306a347440fd40ee6e4717f9416ad1
shurabon <shurabon@gmail.com>
2016-11-24 17:10:44 +0500

    Добавил вызов dbus в propx.sh Убрал все упоминания про pointer в sxhkdrc

--- .config/bspwm/propx.sh
+++ .config/bspwm/propx.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-PROP_X=`xprop |awk '/WM_CLASS/{print $4}'`; notify-send  -t 10000 -u normal "$PROP_X"
+PROP_X=`xprop |awk '/WM_CLASS/{print $4}'`; dbus-launch notify-send  -t 10000 -u normal "$PROP_X"
