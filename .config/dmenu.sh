54290327f47ab803ed785b98e9c0d8b2c5d35a03
shurabon <shurabon@gmail.com>
2017-03-12 18:27:36 +0500

    add dmenu.sh

--- .config/dmenu.sh
+++ .config/dmenu.sh
@@ -1,1 +1,6 @@
-/home/shu/.themes/color_other/dmenu.sh
+#!/bin/bash
+
+COL0=$(xrdb -q | grep color0: | awk '{print $2}')
+COL1=$(xrdb -q | grep color1: | awk '{print $2}')
+
+dmenu_recent -y 350 -h 25 -x 300 -w 800 -o 0.75 -fn "Ubuntu-12:normal" -nb $COL0 -nf '#aaa' -sb $COL1 -sf $COL0
